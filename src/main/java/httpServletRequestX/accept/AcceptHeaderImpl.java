0618c4079524894ddef2534320d993d2dd2827db
mreinwarth <mreinwarth@vz.net>
2012-09-02 17:05:56 +0200

    renamings

--- src/main/java/httpServletRequestX/accept/AcceptHeaderImpl.java
+++ src/main/java/httpServletRequestX/accept/AcceptHeaderImpl.java
@@ -14,12 +14,12 @@ public class AcceptHeaderImpl implements AcceptHeader {
     private static final String           TYPE_JSON            = "application/json";
 
     private String                        content;
-    private final AcceptContenTypeList    contentTypes;
+    private final AcceptContenTypeList    contentTypeList;
     private final AcceptContenTypeFactory contentTypeFactory;
 
     @Inject
-    public AcceptHeaderImpl(AcceptContenTypeList contentTypes, AcceptContenTypeFactory contentTypeFactory) {
-        this.contentTypes = contentTypes;
+    public AcceptHeaderImpl(AcceptContenTypeList contentTypeList, AcceptContenTypeFactory contentTypeFactory) {
+        this.contentTypeList = contentTypeList;
         this.contentTypeFactory = contentTypeFactory;
     }
 
@@ -30,51 +30,51 @@ public class AcceptHeaderImpl implements AcceptHeader {
     }
 
     public String getTop() {
-        if (!contentTypes.isEmpty()) {
-            return contentTypes.get(0).getType();
+        if (!contentTypeList.isEmpty()) {
+            return contentTypeList.get(0).getType();
         }
         return null;
     }
 
     public AcceptContenTypeList getContentTypes() {
-        return contentTypes;
+        return contentTypeList;
     }
 
     public boolean acceptType(String type) {
         String[] typeElements = type.split("/");
         if (typeElements.length > 0) {
-            return contentTypes.containsType(type) || hasWildcard(typeElements[0]) || hasWildcard();
+            return contentTypeList.containsType(type) || hasWildcard(typeElements[0]) || hasWildcard();
         } else {
-            return contentTypes.containsType(type) || hasWildcard();
+            return contentTypeList.containsType(type) || hasWildcard();
         }
     }
 
     public boolean acceptHtml() {
-        return contentTypes.containsType(TYPE_HTML) || hasTextWildcard() || hasWildcard();
+        return contentTypeList.containsType(TYPE_HTML) || hasTextWildcard() || hasWildcard();
     }
 
     public boolean acceptJson() {
-        return contentTypes.containsType(TYPE_JSON) || hasApplicationWildcard() || hasWildcard();
+        return contentTypeList.containsType(TYPE_JSON) || hasApplicationWildcard() || hasWildcard();
     }
 
     private boolean hasWildcard() {
-        return contentTypes.containsType(TYPE_ALL);
+        return contentTypeList.containsType(TYPE_ALL);
     }
 
     private boolean hasWildcard(String first) {
-        return contentTypes.containsType(first + "/*");
+        return contentTypeList.containsType(first + "/*");
     }
 
     private boolean hasTextWildcard() {
-        return contentTypes.containsType(TYPE_ALL_TEXT);
+        return contentTypeList.containsType(TYPE_ALL_TEXT);
     }
 
     private boolean hasApplicationWildcard() {
-        return contentTypes.containsType(TYPE_ALL_APPLICATION);
+        return contentTypeList.containsType(TYPE_ALL_APPLICATION);
     }
 
     private void parseContent() {
-        this.contentTypes.clear();
+        this.contentTypeList.clear();
 
         String[] contentTypes = content.split(",");
         for (String contentType : contentTypes) {
@@ -86,13 +86,13 @@ public class AcceptHeaderImpl implements AcceptHeader {
 
             if (splittedContenttype.length == 2) {
                 Float quality = parseQuality(splittedContenttype[1]);
-                this.contentTypes.add(contentTypeFactory.get(splittedContenttype[0], quality));
+                this.contentTypeList.add(contentTypeFactory.get(splittedContenttype[0], quality));
             } else {
-                this.contentTypes.add(contentTypeFactory.get(splittedContenttype[0]));
+                this.contentTypeList.add(contentTypeFactory.get(splittedContenttype[0]));
             }
         }
 
-        java.util.Collections.sort(this.contentTypes);
+        // java.util.Collections.sort(this.contentTypeList);
     }
 
     private boolean hasParseableContentType(String[] contentTypeElements) {
